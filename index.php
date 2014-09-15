d77781255f54c0f25d4010f030f0d86e54a79130
scotchbitman <blieux.alexandre@gmail.com>
2014-09-16 01:13:30 +0200

    new commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" media="all" href="basic.css" />
+		<link rel="stylesheet" type="text/css" media="all" href="thumbpicker.css" />
+		<script type="text/javascript" src="//code.jquery.com/jquery-2.1.1.min.js"></script>
+	</head>
+	
+	<body>
+	
+		<div class="container">
+		<div class="thumbpicker-frame">
+			<img class="thumbpicker" id="plage" src="thumb/01.jpg" />
+		</div>
+		</div>
+		
+		
+		<div>&nbsp;</div>
+		
+		<div class="container">
+		<div class="thumbpicker-frame">
+			<img class="thumbpicker" id="orage" src="thumb/02.jpeg" />
+		</div>
+		</div>
+		
+		
+		<div>&nbsp;</div>
+		
+		<div id="ctrl">
+			<form id="fo" name="picker" method="post">
+				<input type="submit" value="Envoyer" />
+			</form>
+		</div>
+		
+		<div><pre><?php if(!empty($_POST)) print_r($_POST); ?></pre></div>
+	
+		<script type="text/javascript" src="jquery.thumbpicker.js"></script>
+		<script type="text/javascript">
+			$('.thumbpicker-frame').thumbpicker();
+		</script>
+	</body>
+</html>
